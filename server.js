3b4444555c167814125851a6c1d43de4e53bd7a3
Nakul dev Khajuria <nakuldevkhajuria@gmail.com>
2022-11-10 21:27:46 +0530

    updated till cors installation

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express();
 const data1 = require('./Routes/Bollywood')
+const cors = require('cors')
 
+app.use(cors);
 app.use('/home',data1)
 app.listen(process.env.PORT||8080,() => {
     console.log('server running')

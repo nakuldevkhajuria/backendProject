7a6734346fec14705e0c6ad2254bad1788af0d58
Nakul dev Khajuria <nakuldevkhajuria@gmail.com>
2022-11-10 21:10:49 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const data1 = require('./Routes/bollywood')
+const data1 = require('./Routes/Bollywood')
 
 app.use('/home',data1)
 app.listen(process.env.PORT||8080,() => {
